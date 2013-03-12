dd01c2799c5d9697730478ea2b3606548bb630a1
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-03-12 14:47:47 +0100

    Added missing definitions

--- postprocessing/tamip-cmor.F90
+++ postprocessing/tamip-cmor.F90
@@ -2,10 +2,13 @@
 
  PROGRAM CMORIZETAMIP
      USE netcdf
+     USE cmor_users_functions
      USE NDATASET
      USE CMOR_TAMIP_ROUTINES
 
      IMPLICIT NONE
+     INTEGER :: error_flag
+
      inpath="44.128_tmp_mean.nc"
      error_flag = cmor_setup(inpath=inpath, netcdf_file_action=CMOR_APPEND)
 END PROGRAM CMORIZETAMIP
