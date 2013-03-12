b4bb260b13c422cc758f746e4c4711f63b9425be
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-03-12 11:23:09 +0100

    Added header files used in original grib2cmor code

---
+++ postprocessing/namctl.h
@@ -0,0 +1,4 @@
+NAMELIST /NAMCTL/ NYEAR0, NYEAR, NMONTH, NMDAYS, NMININT, NPRINTLEV, LWGRIB, &
+                & suffix_mon_sfc, suffix_mon_plev, suffix_mon_mlev, &
+                & suffix_day_sfc, suffix_day_plev, &
+                & suffix_3h_sfc, suffix_6h_plev, suffix_6h_mlev
