e0d0057af63c48f5fad69df4518d82586e7fdff3
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-03-13 13:36:35 +0100

    Refactoring of subroutine calls and by putting information into namelists

--- postprocessing/tamip-cmor.F90
+++ postprocessing/tamip-cmor.F90
@@ -5,39 +5,59 @@
      USE CMOR_TAMIP_ROUTINES
 
      IMPLICIT NONE
-     INTEGER :: error_flag, ncid, rhVarId, &
+     INTEGER :: error_flag, ncid, rhVarId, timeVarId, &
                 lonDimID, latDimId, timeDimId, &
                 numLons, numLats, numTimes, numLength, &
                 status , id, ndims
      integer, dimension(nf90_max_var_dims) :: dimIDs
-     character(len=128) :: dim_name
+     character(len=128) :: dim_name, calendar_read
      real, dimension(:, :, :), allocatable :: rhValues
+     DOUBLE PRECISION, dimension(:), allocatable :: alats, alons, time
      INTEGER, PARAMETER :: lon = 512     ! number of longitude grid cells
      INTEGER, PARAMETER :: lat = 256     ! number of latitude grid cells
      INTEGER, PARAMETER :: levs = 16     ! number of standard pressure
 
      call read_nml()
-     INPATH = "44.128_tmp_mean.nc"
      status = cmor_setup(inpath=INPATH, netcdf_file_action=CMOR_APPEND)
      IF (status .ne. 0) call handle_err(status, "CMOR_SETUP")
 
-     status = nf90_open(path = "44.128_tmp_mean.nc", mode = nf90_nowrite, ncid = ncid)
+     status = nf90_open(path = curr_file, mode = nf90_nowrite, ncid = ncid)
      if (status /= nf90_noerr) call handle_err(status, "NF90_OPEN")
 
      status = nf90_inq_varid(ncid, "ES", rhVarId)
      if(status /= nf90_NoErr) call handle_err(status, "NF90_INQ_VARID")
 
-     status = nf90_inquire_variable(ncid, rhVarId, dimids = dimIDs, ndims=ndims)
-     if(status /= nf90_NoErr) call handle_err(status, "NF90_INQUIRE_VARIABLE")
-     do id=1, ndims
-         write(*,*) "size(dimIDs)",size(dimIDs, 1)
-         status = nf90_inquire_dimension(ncid, dimIDs(id), name=dim_name, len = numLength)
-         if(status /= nf90_NoErr) call handle_err(status, "NF90_INQUIRE_DIMENSION")
-         write(*,'(A, I2, 1X, A, A, I3)') "dim_id=",id, dim_name, " length=", numLength
-     end do
-call abort("exiting...")
+     status = nf90_inq_varid(ncid, "time", timeVarId)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_INQ_VARID")
 
-     allocate(rhValues(numLons, numLats, numTimes))
+     status = nf90_get_att(ncid, timeVarId, "calendar", calendar_read)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_ATT")
+
+write(*,*) "calendar=",trim(calendar_read)
+
+     status = cmor_dataset(outpath=outpath,                           &
+                           experiment_id=experiment_id,               &
+                           institution=institution,                   &
+                           source=source,                             &
+                           calendar=calendar_read,                    &
+                           realization=realization,                   &
+                           contact=contact,                           &
+                           history=history,                           &
+                           comment=comment,                           &
+                           references=references,                     &
+                           model_id=model_id,                         &
+                           forcing=forcing,                           &
+                           institute_id=institute_id,                 &
+                           parent_experiment_id=parent_experiment_id, &
+                           branch_time=branch_time,                   &
+                           parent_experiment_rip=parent_experiment_rip)
+
+
+     call read_1d_coord(ncid, rhVarId, "lat", alats)
+     call read_1d_coord(ncid, rhVarId, "lon", alons)
+     call read_1d_coord(ncid, rhVarId, "time", time)
+
+     allocate(rhValues(size(alons), size(alats), size(time)))
      status = nf90_get_var(ncid, rhVarId, rhValues)
      if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_VAR")
 
