e9c1c85e4bb3005ece69d5556873e7148b686218
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-03-22 15:47:21 +0100

    cdo-part is fully scripted(?)

--- postprocessing/postproc.py
+++ postprocessing/postproc.py
@@ -22,15 +22,18 @@ import subprocess
 import sys
 import postproc_aux
 
+# Where am I?
+curr_file = os.path.abspath(os.path.join(os.getcwd(), __file__))
+base_dir  = re.search("(.*)/.*?", os.path.dirname(curr_file)).group(1)
 
 # Input file
 if len(sys.argv) != 2:
     print "Usage: ./postproc.py <defintion_file>"
     sys.exit(1)
-input_file = sys.argv[1]
+definition_file = sys.argv[1]
 
 # Read all lines
-f = open(input_file, "r")
+f = open(definition_file, "r")
 grib_entries = f.readlines()
 f.close()
 
@@ -53,13 +56,17 @@ entries = [[grib_entry[start:end].strip() for start,end in indices] for grib_ent
 # Couple headers to each entry
 params = [dict(zip(headers, param)) for param in entries]
 
-f = open("../def/TAMIP_dates_and_hours.txt", "r")
-dates_hours = f.readlines()
-dates = [date_hh.split()[0] for date_hh in dates_hours]
-hours = [date_hh.split()[1] for date_hh in dates_hours]
-date_hh = dict(zip(dates, hours))
+f = open(os.path.join(base_dir, "def/TAMIP_experiment_info.txt"), "r")
+exp_info_raw = f.readlines()
+exp_info = [entry for entry in exp_info_raw if not re.search('^#', entry)]
+dates = [date_hh.split()[0] for date_hh in exp_info]
+hours = [date_hh.split()[1] for date_hh in exp_info]
+realizations = [date_hh.split()[2] for date_hh in exp_info]
 
-base_folder = "/nobackup/rossby15/rossby/joint_exp/tamip"
+date_hh = dict(zip(dates, hours))
+date_rea = dict(zip(dates, realizations))
+
+model_data_folder = "/nobackup/rossby15/rossby/joint_exp/tamip"
 run_folders = ("TMIP_2008-10-15", "TMIP_2008-10-16", "TMIP_2008-10-17", 
                "TMIP_2008-10-18", "TMIP_2008-10-20", "TMIP_2008-10-21", 
                "TMIP_2008-10-22", "TMIP_2008-10-23", "TMIP_2008-10-25", 
@@ -85,48 +92,55 @@ run_folders = ("TMIP_2008-10-15", "TMIP_2008-10-16", "TMIP_2008-10-17",
 
 run_folder = run_folders[1]
 curr_date = re.sub("TMIP_", "", run_folder)
-os.chdir(os.path.join(base_folder, run_folder))
-gg_files = glob.glob('ICMGG*200*')
-TMIP_match = re.search('ICMGGTM([0-9]{2})\+(200[89])([0-9]{2})', gg_files[0])
+os.chdir(os.path.join(model_data_folder, run_folder))
+
+# Check which grib files are present in this folder
+grib_files = glob.glob('ICMGG*200*')
+TMIP_match = re.search('ICM..TM([0-9]{2})\+(200[89])([0-9]{2})', grib_files[0])
 TMIP_index = TMIP_match.group(1)
 TMIP_year = TMIP_match.group(2)
 TMIP_month = TMIP_match.group(3)
 
 
-if len(gg_files) == 1:  #  The normal case, a singel grib file where we split the entries
-    cdo_splitparam = "cdo splitparam " + gg_files[0] + " split"
-    cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
-    subprocess.check_call(["rename split '' split*"], shell=True)
-elif len(gg_files) == 2:  # Special case if grib files are split across two months
-    TMIP_match2 = re.search('ICMGGTM([0-9]{2})\+(200[89])([0-9]{2})', gg_files[1])
-    TMIP_index2 = TMIP_match2.group(1)
-    TMIP_year2 = TMIP_match2.group(2)
-    TMIP_month2 = TMIP_match2.group(3)
-    print TMIP_index2, TMIP_year2, TMIP_month2
-    cdo_splitparam = "cdo splitparam " + gg_files[0] + " " + TMIP_month + "."
-    cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
-    cdo_splitparam = "cdo splitparam " + gg_files[1] + " " + TMIP_month2 + "."
-    cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
-    split_files = glob.glob(TMIP_month + '*')
-    for split_file in split_files:
-        other_split_file = re.sub(TMIP_month, TMIP_month2, split_file)
-        merged_file = re.sub("^" + TMIP_month + "\.", "", split_file)
-        cdo_mergetime = "cdo mergetime " + split_file + " " + other_split_file + " " + merged_file
-        cdo_command = postproc_aux.cdo_launch(cdo_mergetime, log_handle=sys.stdout)
-        os.remove(split_file)
-        os.remove(other_split_file)
-else:
-    print "*EE*: Script can only handle two separate GRIB-files..."
-    raise
+# Split IFS-grib files into files named "paramId.table2Version"
+## if len(grib_files) == 1:  #  The normal case, a single grib file where we split the entries
+##     cdo_splitparam = "cdo splitparam " + grib_files[0] + " split"
+##     cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
+##     subprocess.check_call(["rename split '' split*"], shell=True)
+## elif len(grib_files) == 2:  # Special case if grib files are split across two months
+##     TMIP_match2 = re.search('ICM??TM([0-9]{2})\+(200[89])([0-9]{2})', grib_files[1])
+##     TMIP_index2 = TMIP_match2.group(1)
+##     TMIP_year2 = TMIP_match2.group(2)
+##     TMIP_month2 = TMIP_match2.group(3)
+##     print TMIP_index2, TMIP_year2, TMIP_month2
+##     cdo_splitparam = "cdo splitparam " + grib_files[0] + " " + TMIP_month + "."
+##     cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
+##     cdo_splitparam = "cdo splitparam " + grib_files[1] + " " + TMIP_month2 + "."
+##     cdo_command = postproc_aux.cdo_launch(cdo_splitparam, log_handle=sys.stdout)
+##     split_files = glob.glob(TMIP_month + '*')
+##     for split_file in split_files:
+##         other_split_file = re.sub(TMIP_month, TMIP_month2, split_file)
+##         merged_file = re.sub("^" + TMIP_month + "\.", "", split_file)
+##         cdo_mergetime = "cdo mergetime " + split_file + " " + other_split_file + " " + merged_file
+##         cdo_command = postproc_aux.cdo_launch(cdo_mergetime, log_handle=sys.stdout)
+##         os.remove(split_file)
+##         os.remove(other_split_file)
+## else:
+##     print "*EE*: Script can only handle one or two separate GRIB-files..."
+##     raise
 
 for param in params:
-    cdo_grb2nc_parSel = "cdo -R -r -f nc -t ecmwf copy " + param['variablesGG'] + ".grb " + param['variablesGG'] + "_tmp.nc"
-    cdo_command = postproc_aux.cdo_launch(cdo_grb2nc_parSel, log_handle=sys.stdout)
-
-    cdo_3h_mean = "cdo timselmean,4 " + param['variablesGG'] + "_tmp.nc " + param['variablesGG'] + "_tmp_mean.nc"
-    cdo_command = postproc_aux.cdo_launch(cdo_3h_mean, log_handle=sys.stdout)
+    param_def_file = os.path.join(base_dir, "def", param['variablesGG'] + ".def")
+    if (os.path.exists(param_def_file)):
+        param_file = open(param_def_file, "r")
+        param_def = param_file.readlines()
+        for param_def_line in param_def:
+            cdo_command = param_def_line
+            cdo_return = postproc_aux.cdo_launch(cdo_command, log_handle=sys.stdout)
 
     cdo_setreftime = "cdo setreftime," + curr_date + "," + date_hh[curr_date] + ":00 " + param['variablesGG'] + "_tmp_mean.nc " + param['variablesGG'] + "_tmp_mean_reftime0.nc"
     cdo_command = postproc_aux.cdo_launch(cdo_setreftime, log_handle=sys.stdout)
+
+    
 
     sys.exit(1)
