6a1d2f68b5078215fa327b80e3df2cc7d8a3a448
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-11-08 15:02:23 +0100

    Working setup for Pressure level table

---
+++ postprocessing/TAMIP_3hrPlev.F90
@@ -0,0 +1,118 @@
+ PROGRAM CMORIZETAMIP
+     USE netcdf
+     USE level_coefficients
+     USE cmor_users_functions
+     USE NCMORIO
+     USE NATTRIBUTES
+     USE CMOR_TAMIP_ROUTINES
+
+     IMPLICIT NONE
+     INTEGER :: error_flag, ncid, ncid2, rhVarId, timeVarId, &
+                lonDimID, latDimId, timeDimId, levVarId, &
+                numLons, numLats, numTimes, numLength, &
+                status , id, ndims, tableId, ilon, ilat, itime, i, &
+                cvar, counter, ilev
+     integer, dimension(nf90_max_var_dims) :: dimIDs
+
+     character(len=128) :: dim_name, calendar_read, units_read
+     character(len=1024) :: history
+     character (len=*) , parameter :: end_of_line = char(13)//char(10)
+     real, dimension(:, :, :, :), allocatable :: rhValues
+     DOUBLE PRECISION, dimension(:), allocatable :: alats, alons, time, levels
+     DOUBLE PRECISION, dimension(:, :), allocatable :: alats_bounds, alons_bounds, time_bounds, levels_bounds
+
+     call read_nml()
+     status = cmor_setup(inpath=INPATH, netcdf_file_action=CMOR_APPEND)
+     IF (status .ne. 0) call handle_err(status, "CMOR_SETUP")
+
+     tableId = cmor_load_table(table_id)
+     call cmor_set_table(tableId)
+
+     status = nf90_open(path = curr_file, mode = nf90_nowrite, ncid = ncid)
+     if (status /= nf90_noerr) call handle_err(status, "NF90_OPEN")
+
+     status = nf90_inq_varid(ncid, model_varname, rhVarId)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_INQ_VARID (model_varname)")
+
+     status = nf90_inq_varid(ncid, "time", timeVarId)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_INQ_VARID (time)")
+
+     status = nf90_get_att(ncid, timeVarId, "calendar", calendar_read)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_ATT")
+
+     status = nf90_get_att(ncid, NF90_GLOBAL, "history", history)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_ATT")
+     history=trim(history)//end_of_line
+
+     status = nf90_get_att(ncid, timeVarId, "units", units_read)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_ATT")
+
+     status = cmor_dataset(branch_time=branch_time,                   &
+                           calendar=calendar_read,                    &
+                           comment=comment,                           &
+                           contact=contact,                           &
+                           experiment_id=experiment_id,               &
+                           forcing=forcing,                           &
+                           history=history,                          &
+                           institute_id=institute_id,                 &
+                           institution=institution,                   &
+                           model_id=model_id,                         &
+                           parent_experiment_id=parent_experiment_id, &
+                           parent_experiment_rip=parent_experiment_rip, &
+                           realization=realization,                   &
+                           references=references,                     &
+                           source=source,                             &
+                           outpath=outpath)
+
+
+     call read_1d_coord(ncid, rhVarId, "lat", alats, alats_bounds)
+     call read_1d_coord(ncid, rhVarId, "lon", alons, alons_bounds)
+     call read_1d_coord(ncid, rhVarId, "time", time, time_bounds)
+     call read_1d_coord(ncid, rhVarId, "lev", levels, levels_bounds)
+
+     allocate(rhValues(size(alons), size(alats), size(levels), size(time)))
+     status = nf90_get_var(ncid, rhVarId, rhValues)
+     if(status /= nf90_NoErr) call handle_err(status, "NF90_GET_VAR")
+
+     status = nf90_close(ncid)
+     if (status /= nf90_noerr) call handle_err(status, "NF90_CLOSE")
+
+     ilon = cmor_axis(table_entry='longitude', &
+                      units='degrees_east',    &
+                      coord_vals=alons,        &
+                      cell_bounds=alons_bounds)
+     if (ilon .lt. 0) call handle_err(status, "CMOR_AXIS_LON")
+
+     ilat = cmor_axis(table_entry='latitude', &
+                      units='degrees_north',  &
+                      coord_vals=alats,       &
+                      cell_bounds=alats_bounds)
+     if (ilat .lt. 0) call handle_err(status, "CMOR_AXIS_LAT")
+
+
+     itime = cmor_axis(table_entry='time',     &
+                       units=units_read,       &
+                       coord_vals=time,        &
+                       interval="180 minutes", &
+                       cell_bounds=time_bounds)
+     if (itime .lt. 0) call handle_err(status, "CMOR_AXIS_TIME")
+
+     ilev = cmor_axis(table_entry='plev8',     &
+                       units='Pa',       &
+                       coord_vals=levels)
+     if (ilev .lt. 0) call handle_err(status, "CMOR_AXIS_LEVEL")
+
+     cvar = cmor_variable(table_entry=cmor_varname,       &
+                          units=model_units,              &
+                          axis_ids=(/ilon, ilat, ilev, itime/), &
+                          missing_value=1.0e20,           &
+                          positive=positive,              &
+                          original_name=original_name)
+
+      status = cmor_write(var_id = cvar,    &
+                          data   = rhValues)
+     if (status /= 0) call handle_err(status, "CMOR_WRITE")
+
+     status = cmor_close()
+     if (status /= 0) call handle_err(status, "CMOR_CLOSE")
+END PROGRAM CMORIZETAMIP
